bb166f646abdaf74d0368a1083eb4225b7d3d96d
moskalyk <morgan.moskalyk@protonmail.ch>
2023-10-10 21:36:40 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,46 +1,9 @@
-# Getting Started with Create React App
+# localducts 💧
+probably a year ago i had the idea to create a networked tearduct app, so you can log or share when your eyes have tears. never got around to it. 
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+more recently i was eating a shawarma, and cut my (left) eye with the tinfoil. it's been tearing for a full day.
 
-## Available Scripts
+localducts is an expierment to log the health of my eye during recovery, and could be useful for others in the world during: violent times and / or to appreciate world mental health day (today)
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+# user story
+as a user I want to select an eye and input a measurement percentage of the amount of tears that are in your eye all within 7 seconds (to align with evm mainnet blocktimes) all stored localstorage so that I can track trends of eyeduct moisture
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,90 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Eyezz = () => {
+
+  const [left, setLeft] = useState(false)
+  const [right, setRight] = useState(false)
+  const [measurement, setMeasurement] = useState(0)
+
+  React.useEffect(() => {
+    let interval = setInterval(() => {
+      window.dispatchEvent(new Event("storage"))
+
+      console.log(measurement)
+      if(measurement != 0){
+        if(localStorage.getItem('nonce:log')=== null) localStorage.setItem('nonce:log', String(1))
+        else localStorage.setItem('nonce:log', String(Number(localStorage.getItem('nonce:log'))+1))
+      
+        localStorage.setItem(`list:${localStorage.getItem('nonce:log')}`, JSON.stringify({measurement: measurement, left: left ? left : right}))
+      }
+      setMeasurement(0)
+      setLeft(false)
+      setRight(false)
+    }, 7000)
+
+    return () => clearInterval(interval)
+  }, [measurement])
+
+  
+
+  return (
+    <div className="center-container">
+      <div className="svg-container">
+        <div className="svg" onClick={() => setLeft(true)}>
+          <svg style={{ fill: left ? 'black' : 'grey' }}  xmlns="http://www.w3.org/2000/svg" width="244" height="244" viewBox="0 0 24 24"><path d="M15 12c0 1.654-1.346 3-3 3s-3-1.346-3-3 1.346-3 3-3 3 1.346 3 3zm9-.449s-4.252 8.449-11.985 8.449c-7.18 0-12.015-8.449-12.015-8.449s4.446-7.551 12.015-7.551c7.694 0 11.985 7.551 11.985 7.551zm-7 .449c0-2.757-2.243-5-5-5s-5 2.243-5 5 2.243 5 5 5 5-2.243 5-5z"/></svg>
+        </div>
+        <div className="svg" onClick={() => setRight(true)}>
+          <svg style={{ fill:  right ? 'black' : 'grey' }} xmlns="http://www.w3.org/2000/svg" width="244" height="244" viewBox="0 0 24 24"><path d="M15 12c0 1.654-1.346 3-3 3s-3-1.346-3-3 1.346-3 3-3 3 1.346 3 3zm9-.449s-4.252 8.449-11.985 8.449c-7.18 0-12.015-8.449-12.015-8.449s4.446-7.551 12.015-7.551c7.694 0 11.985 7.551 11.985 7.551zm-7 .449c0-2.757-2.243-5-5-5s-5 2.243-5 5 2.243 5 5 5 5-2.243 5-5z"/></svg>
+        </div>
+      </div>
+      <div className="input-container">
+        <input  value={measurement}type="text" placeholder="1-100" onChange={(evt: any) => setMeasurement(evt.target.value)} />
+      </div>
+    </div>
+  );
+}
+
+const Shelf = () => {
+
+  const [logList, setLogList] = useState([])
+
+  React.useEffect(() => {
+    const listener = () => {
+
+      const nonceCID = localStorage.getItem("nonce:log") ? localStorage.getItem("nonce:log")! : 0
+      const listNames: any = []
+      console.log(nonceCID)
+      // iterate over localstorage cids
+      for(let i = 0; i < Number(nonceCID); i++){
+        listNames.push(<li style={{cursor: 'pointer', margin: '10px'}}>{JSON.parse(localStorage.getItem(`list:${i+1}`)!).left ? 'L' : 'R'} {JSON.parse(localStorage.getItem(`list:${i+1}`)!).measurement}</li>)
+      }
+
+      // set cids
+      setLogList(listNames)
+    };
+    window.addEventListener("storage", listener);
+    return () => {
+      window.removeEventListener("storage", listener);
+    };
+  }, [])
+
+  return(
+    <>
+      <div style={{textAlign: 'center'}}>
+        {logList}
+      </div>
+    </>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Eyezz/>
+      <Shelf/>
+    </>
   );
 }
 
